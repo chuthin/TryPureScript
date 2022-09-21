acce9b62f6dee5dc3ff6288653d2f4f8f743fbd3
chuthin <thincv@live.com>
2022-09-21 22:42:43 +0700

    Create main.purs

---
+++ main.purs
@@ -0,0 +1,14 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+import Data.Array ((..))
+import Data.Foldable (for_)
+import TryPureScript (render, withConsole)
+
+main :: Effect Unit
+main = render =<< withConsole do
+  for_ (10 .. 1) \n -> log (show n <> "...")
+  log "end!"
