7306d171b0e1947fec76c174e7ca5a06bafbb655
chuthin <thincv@live.com>
2022-09-22 16:43:13 +0700

    Update typeClass.purs

--- typeClass.purs
+++ typeClass.purs
@@ -1,1 +1,11 @@
 
+
+module Main where
+
+import Prelude
+import Effect.Console (log)
+import Data.List (List(..), (:))
+import Data.Foldable (fold)
+import Data.Maybe
+import Effect (Effect)
+import TryPureScript (h1, h2, p, text, list, indent, link, render, code)
