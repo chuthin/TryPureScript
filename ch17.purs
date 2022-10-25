73309ea9700d817d9f4d0a4ecc80c674e8e4e7ad
chuthin <thincv@live.com>
2022-10-25 23:10:50 +0700

    Update ch17.purs
    Add Validation

--- ch17.purs
+++ ch17.purs
@@ -4,10 +4,12 @@ import Prelude
 
 import Data.Foldable (fold)
 import Effect (Effect)
+import Data.Newtype (class Newtype)
 import Data.Generic.Rep
 import Data.Show.Generic
 import Data.Functor
 import Data.Bifunctor
+import Data.Semigroup
 import Control.Apply
 import Control.Applicative
 import Effect.Console (log)
@@ -59,15 +61,36 @@ fullName :: String -> String -> String -> String
 fullName firstName midName lastName = firstName <> " " <> midName <> " " <> lastName
 
 errorIfMissing :: Maybe String -> String -> Either String String
-errorIfMissing Nothing error = Right error
-errorIfMissing (Just x) _ = Left x
+errorIfMissing Nothing error = Left error
+errorIfMissing (Just x) _ = Right x
 
 fullNameEither :: Maybe String -> Maybe String -> Maybe String -> Either String String
 fullNameEither firstName midName lastName = fullName <$> errorIfMissing firstName "Fisrt name must exist"
                                                      <*> errorIfMissing midName "Mid name must exist"
                                                      <*> errorIfMissing lastName "Last name must exist"
 
+newtype Validation err result = Validation (Either err result)
+derive instance newtypeValidation :: Newtype (Validation err result) _
+derive newtype instance functorValidation :: Functor (Validation err)
+derive newtype instance bifunctionValidation :: Bifunctor Validation
+derive newtype instance eqValidation :: (Eq err, Eq result) => Eq (Validation err result)
+derive newtype instance ordValidation :: (Ord err, Ord result) => Ord (Validation err result)
+derive instance genericValidation :: Generic (Validation err result) _
+derive newtype instance showValidation :: (Show err, Show result) => Show (Validation err result) 
 
+instance applyValidation:: Semigroup err => Apply (Validation err) where
+  apply (Validation (Left err1)) (Validation (Left err2)) = Validation $ Left (err1 <> err2)
+  apply (Validation (Left err)) _ = Validation $ Left err
+  apply (Validation (Right f)) x = f <$> x
+
+errorIfMissing' :: Maybe String -> String -> Validation (Array String) String
+errorIfMissing' Nothing error = Validation $ Left [error]
+errorIfMissing' (Just x) _ = Validation $ Right x
+
+fullNameEither' :: Maybe String -> Maybe String -> Maybe String -> Validation (Array String) String
+fullNameEither' firstName midName lastName = fullName <$> errorIfMissing' firstName "Fisrt name must exist"
+                                                     <*> errorIfMissing' midName "Mid name must exist"
+                                                     <*> errorIfMissing' lastName "Last name must exist"
 
 main :: Effect Unit
 main = do 
@@ -80,5 +103,6 @@ main = do
   log $ show $ (Left 22 :: (Either Int String))
   log $ show $ ((+) <$> pure 1 <*> (pure identity <*> pure 2 ):: Either Unit Int)
   log $ show $ fullNameEither (Just "Chu") (Just "Van") (Just "Thin")
-  log $ show $ fullNameEither (Just "Nguyen") Nothing (Just "Thin")
-  
+  log $ show $ fullNameEither Nothing Nothing (Just "Thin")
+  log $ show $ fullNameEither' Nothing Nothing (Just "Thin")
+  log $ show $ fullNameEither' (Just "Chu") (Just "Van") (Just "Thin")
